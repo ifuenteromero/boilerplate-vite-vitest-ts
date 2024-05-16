fbe04899ddc16953436705ee0102b713b63a7a16
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-05-16 17:23:23 +0200

    prettier npm i -D prettier Create .prettierrc Format all files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 1- npm create vite@latest
+2- npm i -D prettier. Create .prettierrc. Format all files
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 const App = () => {
-	return <h1>boilerplate-vite-vitest-ts</h1>;
+    return <h1>boilerplate-vite-vitest-ts</h1>;
 };
 
 export default App;
