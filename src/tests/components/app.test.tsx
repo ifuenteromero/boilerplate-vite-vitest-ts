da87d78b08006d093dd677dfdbff7f36afe3a491
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-05-20 14:16:52 +0200

    Simplifying tests: import '@testing-library/jest-dom/vitest';

--- src/tests/components/app.test.tsx
+++ src/tests/components/app.test.tsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom/vitest'; // Si no, da error los matchers ej toBeInTheDocument
 import { render, screen } from '@testing-library/react';
 import App from '../../components/App';
 import { projectName } from '../../projectName';
