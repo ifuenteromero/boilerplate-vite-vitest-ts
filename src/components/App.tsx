3d027358a8c43450ab863841c2696f6c60bb9264
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-05-20 14:35:17 +0200

    Install @testing-library/user-event - npm i -D @testing-library/user-event - Add sample test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
 import { projectName } from '../projectName';
 
 const App = () => {
-    return <h1>{projectName}</h1>;
+    const [text, setText] = useState('on');
+    const handleClick = () =>
+        setText((prevText) => (prevText === 'on' ? 'off' : 'on'));
+
+    return (
+        <>
+            <h1>{projectName}</h1>
+            <button onClick={handleClick}>{text}</button>
+        </>
+    );
 };
 
 export default App;
